78a458cef59296f424750197bc9a4efa502e1a6a
Harshit-Wadichar <manohar.wadichar9545@gmail.com>
2025-11-21 23:09:14 +0530

    learning data types

--- data-types/index.js
+++ data-types/index.js
@@ -62,3 +62,38 @@ literalVar = "success";
 function add(a, b) {
     return a + b;
 }
+//------------------------number types--------------------------
+//var var_name : data_type = value
+var num1 = 10;
+var num2 = 30;
+var num3 = 30;
+var total = num1 + num3;
+console.log(total);
+var oct = 32769;
+var hexa = 1;
+var binary = 0x00001;
+console.log(oct + 10);
+var item = 100;
+var item2 = "50";
+// var item2Converted=Number(item2)
+var item2Converted = +item2;
+console.log(item + +item2Converted);
+// var data =30;
+// data=40
+// data="anil sidhu"
+var data = 30;
+data = "anil";
+//------------------------string and boolean types--------------------------
+var str = "Hello How are you";
+var str2 = 'Hello How are you';
+var str3 = "Hello How are you";
+var age = 30;
+var userName = "Anil sidhu";
+var info = "my name is ".concat(userName, " and age is ").concat(age);
+console.log(info);
+var num = 100;
+// var data:string = num. toString();
+// var data: string = " "+ num;
+var boolData = true;
+// var data:string = boolData. toString();
+var data = "" + boolData;
--- data-types/index.ts
+++ data-types/index.ts
@@ -78,3 +78,50 @@ literalVar="success";
 function add(a:number,b:number):number{
     return a+b;
 }
+
+//------------------------number types--------------------------
+//var var_name : data_type = value
+var num1:number=10;
+var num2:number=30
+var num3:number=30
+
+var total:number=num1+num3;
+console.log(total);
+
+var oct:number=0o100001;
+var hexa:number=0b00001;
+var binary:number=0x00001;
+
+console.log(oct+10);
+
+var item:number=100;
+var item2="50";
+// var item2Converted=Number(item2)
+var item2Converted=+item2
+console.log(item+ +item2Converted);
+
+// var data =30;
+// data=40
+// data="anil sidhu"
+
+var data: number | string = 30;
+data="anil";
+
+
+//------------------------string and boolean types--------------------------
+var str:string="Hello How are you";
+var str2:string='Hello How are you';
+var str3:string=`Hello How are you`;
+
+var age :number=30;
+var userName: string="Anil sidhu";
+
+var info:string= `my name is ${userName} and age is ${age}`;
+console.log(info);
+
+var num:number=100;
+// var data:string = num. toString();
+// var data: string = " "+ num;
+var boolData=true;
+// var data:string = boolData. toString();
+var data: string = "" +boolData;
